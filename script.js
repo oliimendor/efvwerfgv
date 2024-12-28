d05101ad50a5fc60984731d3b68fd54eee6d53c9
oliimendor <endor.sensey@gmail.com>
2024-12-28 16:35:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Boshlangich ayrringiz:4");
+alert("sizning arrayingizdan malumot olib tashlanoshchimiz");
+let fruits = ["Яблоко", "Банан", "Апельсин"];
+
+console.log(fruits);
+
+fruits.push('Jonka');
+
+console.log(fruits);
+
+let qovunKalala = ['AbdAllAh ', 'HadiMal', 'Yusup', 'ABdulazizKrasavchik']
+console.log(qovunKalala);
+
+qovunKalala.pop(qovunKalala);
